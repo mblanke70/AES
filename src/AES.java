6ef75ed8e7439788286ed9b39690f90cfb99d740
Malte Blanke <blanke@ursulaschule.de>
2025-01-22 12:55:40 +0100

    ...

--- src/AES.java
+++ src/AES.java
@@ -1,18 +1,53 @@
 public class AES {
 
     // 10 Runden, 16 Byte Plaintext, 16 Byte = 128 Bit Schlüssel
-    public int[][] states = new int[10][16];
-    public int[][] keys   = new int[10][16];
+    public int[][] states = new int[11][16];
 
-    public AES(String plain, String key) {
+    public int[] key = new int[16];
 
-        for(int i=0; i<plain.length(); i++) {
-            states[0][i] = (int) plain.charAt(i);
-            keys  [0][i] = (int) key.charAt(i);
+    public AES(String p, String k) {
+
+        for(int i=0; i<p.length(); i++) {
+            states[0][i] = (int) p.charAt(i);   // state 0
+            key[i]       = (int) k.charAt(i);   // round key 0
         }
 
         System.out.println( "Plaintext: " + toHex(states[0]) );
-        System.out.println( "Key: " + toHex(keys[0]) );
+        System.out.println( "Key: " + toHex(key) );
+
+        // Rundenschlüssekl generieren (key expansion)
+        /*
+         * Ablauf:
+         *
+         * R = 0
+         * addRoundKey(keys[R])
+         *
+         * wiederhole für R=1 bis 9:
+         *   subBytes
+         *   shiftRows
+         *   mixColumns
+         *   addRoundKey(keys[R])
+         *
+         * subBytes
+         * shiftRows
+         * mixColumns
+         */
+    }
+
+    public int[] addRoundKey(int[] input) {
+        return new int[]{0};
+    }
+
+    public int[] subBytes(int[] input) {
+        return new int[]{0};
+    }
+
+    public int[] shiftRows(int[] input) {
+        return new int[]{0};
+    }
+
+    public int[] mixColumns(int[] input) {
+        return new int[]{0};
     }
 
 
